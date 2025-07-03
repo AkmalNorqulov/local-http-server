655a1f89544e078e6203a3017f54ebc3621a87b4
Akmal Norqulov <akmal9999007@gmail.com>
2025-07-03 23:07:15 +0500

    initial commit

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+    "embed"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "os/signal"
+    "strings"
+    "syscall"
+
+    "github.com/miekg/dns"
+)
+
+//go:embed dist/*
+var embeddedFS embed.FS
+
+const domainName = "sample-page.uz."
+var serverIP = "192.168.0.100" // change this to your real LAN IP
+
+
+func startHTTP() {
+    fs := http.FileServer(http.FS(embeddedFS))
+    http.Handle("/", fs)
+    log.Printf("[HTTP] Serving at http://%s", serverIP)
+    log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func forwardDNS(w dns.ResponseWriter, r *dns.Msg) {
+    c := &dns.Client{}
+    resp, _, err := c.Exchange(r, "8.8.8.8:53")
+    if err != nil {
+        log.Printf("[DNS] Forward error: %v", err)
+        dns.HandleFailed(w, r)
+        return
+    }
+    w.WriteMsg(resp)
+}
+
+func startDNS() {
+    dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
+        m := new(dns.Msg)
+        m.SetReply(r)
+        for _, q := range r.Question {
+            if strings.EqualFold(q.Name, domainName) {
+                rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, serverIP))
+                if err == nil {
+                    m.Answer = append(m.Answer, rr)
+                    log.Printf("[DNS] %s -> %s", q.Name, serverIP)
+                }
+                w.WriteMsg(m)
+                return
+            }
+        }
+        forwardDNS(w, r)
+    })
+    go dns.ListenAndServe(":53", "udp", nil)
+    go dns.ListenAndServe(":53", "tcp", nil)
+    log.Printf("[DNS] Intercepting %s on port 53", domainName)
+}
+
+func main() {
+    log.Println("[AIO] Starting up...")
+    go startDNS()
+    go startHTTP()
+
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    <-c
+    log.Println("[AIO] Stopping")
+}
